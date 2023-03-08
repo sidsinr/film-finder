848151d7504f4dd53e5e1bbbcf1b96cd31fd4b3c
Siddharth Singh <118676744+sidsinr@users.noreply.github.com>
2023-03-08 20:33:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    };
+
+    useEffect(() => {
+        searchMovies("Them");
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Film Finder</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No Movies Found</h2>
+                    </div>
+                )
+            }
+
+        </div>
+    );
+}
+
+export default App;
